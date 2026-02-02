26b55742c483c00e7214bacba6592c96c54cb1dd
rsainiv3 <rahul@v3cars.com>
2026-02-02 14:52:37 +0530

    csv download

--- src/app/(general)/layout.js
+++ src/app/(general)/layout.js
@@ -35,50 +35,52 @@ const Layout = ({ children }) => {
   }
 
   return (
-    <main className="auth-creative-wrapper">
-      <div className="auth-creative-inner">
-        <div className="creative-card-wrapper">
-          <div className="card  overflow-hidden" style={{ zIndex: 1 }}>
-            <div className="row flex-1 g-0">
-              {/* Left Side - 70% White Background with Image */}
-              <div className="col-lg-8 h-100 bg-white order-0 order-lg-0">
-                <div className="h-100 d-flex align-items-center justify-content-center p-5">
-                  <Image
-                    width={499}
-                    height={399}
-                    sizes="100vw"
-                    src="/images/dashboard_logo1.png"
-                    alt="img"
-                    className="img-fluid"
-                  />
-                </div>
-              </div>
-
-              {/* Right Side - 30% with Login Form */}
-              <div className="col-lg-4 h-100 my-auto order-1 order-lg-1 position-relative">
-                {/* Logo positioned at top center */}
-                <div className="text-center pt-4 pb-3">
-                  <img
-                    src="/images/login_logo.png"
-                    alt="logo"
-                    className="img-fluid"
-                    style={{ width: "240px", height: "160px" }}
-                  />
-                </div>
-
-                {/* Login Form */}
-                <div className="creative-card-body card-body px-4 pb-5">
-                  <LoginForm
-                    registerPath={"/authentication/register/creative"}
-                    resetPath={"/authentication/reset/creative"}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+  <main className="auth-creative-wrapper">
+       <div className="auth-creative-inner">
+         <div className="creative-card-wrapper">
+           <div >
+             <div className="row flex-1 g-0">
+               {/* Left Side - 70% White Background with Image */}
+                  <div className="col-lg-7 d-md-block d-sm-none order-0 order-lg-0">
+                              <div className="d-flex align-items-center justify-content-center p-5">
+                                <Image
+                                  width={800}
+                                  height={399}
+                                  sizes="100vw"
+                                  src="/images/dashboard_logo1.png"
+              
+                                  alt="img"
+                                  className="img-fluid"
+                                />
+                              </div>
+                            </div>
+              
+ 
+               {/* Right Side - 30% with Login Form */}
+               <div className="col-lg-5 h-100 my-auto order-1 order-lg-1 position-relative">
+                 {/* Logo positioned at top center */}
+                 <div className="text-center pt-4 pb-3">
+                    <img
+                     src="/images/login_logo.png"
+                     alt="logo"
+                     className="img-fluid"
+                     style={{ width: '240px', marginBottom: '30' }}
+                   />
+                 </div>
+ 
+                 {/* Login Form */}
+                 <div className="creative-card-body card-body px-4 pb-5">
+                   <LoginForm
+                     registerPath={"/authentication/register/creative"}
+                     resetPath={"/authentication/reset/creative"}
+                   />
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </main>
   );
 };
 
